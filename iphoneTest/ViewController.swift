77cc60c3f734f812035ccf76469cea9717b8f024
mechandesuw <w.mick_ovo@ezweb.ne.jp>
2019-04-19 04:45:44 +0900

    Initial Commit

---
+++ iphoneTest/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  iphoneTest
+//
+//  Created by 山谷美咲生 on 2019/04/14.
+//  Copyright © 2019 山谷美咲生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let
+    
+    
+    @IBOutlet var lable: UILabel! //押したところのラベル
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func right() {
+        
+    }
+
+    
+    @IBAction func left() {
+        
+    }
+    
+    @IBAction func number1() {
+        
+    }
+    
+    @IBAction func number () {
+        
+        
+    }
+    
+    
+
+
+}
+
